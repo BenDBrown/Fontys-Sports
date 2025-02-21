b96e2a88b4e7fc8693ac27d828799624ad048f87
mikedriessen <36692429+mikedriessen@users.noreply.github.com>
2025-02-21 13:42:35 +0100

    AddedFootBlocks
    Added the foot feature

---
+++ FontysSports/Assets/FeetSaber/Scripts/FootSaber.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FootSaber : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer)) 
+        { 
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        previousPos = transform.position;
+        
+    }
+}
--- FontysSports/Assets/FeetSaber/Scripts/Spawner.cs
+++ FontysSports/Assets/FeetSaber/Scripts/Spawner.cs
@@ -1,11 +1,20 @@
+using System.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
-{
+{   
+    //Here are the hand spawners
     public GameObject[] cubes;
     public Transform[] points;
     public float beat = (60/105)*2;
     private float timer;
+
+    //Here are the feet spawners
+    public GameObject[] footPlates;
+    public Transform[] footPoints;
+    public float footBeat = (60/105)*2;
+    private float footTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,13 +26,21 @@ public class Spawner : MonoBehaviour
     {
         if (timer > beat)
         {
-            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+            GameObject cube = Instantiate(cubes[Random.Range(0, cubes.Length)], points[Random.Range(0,points.Length)]);
             cube.transform.localPosition = Vector3.zero;
             cube.transform.Rotate(transform.forward, 90 * Random.Range(0,4));
             timer -= beat;
         }
 
         timer += Time.deltaTime;
+
+        if (footTimer > footBeat)
+        {
+            GameObject footCube = Instantiate(footPlates[Random.Range(0,footPlates.Length)], footPoints[Random.Range(0, footPoints.Length)]);
+            footTimer -= footBeat;
+        }
+
+        footTimer += Time.deltaTime;
 
     }
 }
