f27441d0b73ed693aa3fbebd9cbccd1bef919fa9
BenDBrown <benjamin.d.brown.2002@gmail.com>
2025-02-21 15:22:04 +0100

    initial kicking with test script, physics wonky

---
+++ FontysSports/Assets/FootBall/Scripts/TestKick.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TestKick : MonoBehaviour
+{
+    [SerializeField]
+    private Quaternion windBackRotation;
+
+    [SerializeField]
+    private Quaternion windForwardRotation;
+
+    [SerializeField]
+    private float speed = 0.0001f;
+
+    private float time = 0;
+
+    private bool kicking = false;
+
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        Kick();
+    }
+
+    private void Update()
+    {
+        if (kicking)
+        { 
+            transform.rotation = Quaternion.Lerp(windBackRotation, windForwardRotation, time * speed);
+            time += Time.deltaTime;
+            if(transform.rotation == windForwardRotation) StopKicking();
+        }
+    }
+
+    private void Kick()
+    {
+        time = 0;
+        transform.rotation = windBackRotation;
+        kicking = true;
+    }
+
+    private void StopKicking()
+    { 
+        kicking = false;
+        transform.rotation = Quaternion.identity;
+    }
+}
