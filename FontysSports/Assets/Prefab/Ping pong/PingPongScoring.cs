8e7ec979ad506cd16532831a410b758b990c6c05
YoranSiebers <457036@student.fontys.nl>
2025-06-11 21:29:57 +0200

    Added scoring and ball script, added prefabs for scoreboard and table

---
+++ FontysSports/Assets/Prefab/Ping pong/PingPongScoring.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PingPongScoring : MonoBehaviour
+{
+    private static int player1Score = 0;
+    private static int player2Score = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
