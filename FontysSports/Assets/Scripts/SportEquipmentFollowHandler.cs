8377a50fa467aea18a6f56f524d334d0efd0c01c
Robbert Lengton <robbert.lengton@hotmail.nl>
2025-06-12 01:21:51 +0200

    add ability to switch club between controllers

--- FontysSports/Assets/Scripts/SportEquipmentFollowHandler.cs
+++ FontysSports/Assets/Scripts/SportEquipmentFollowHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
@@ -5,28 +6,37 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs;
 public class SportEquipmentFollowHandler : MonoBehaviour
 {
     [SerializeField] private bool rightControllerActive = true;
-    [SerializeField] private Transform leftController;
-    [SerializeField] private Transform rightController;
     [SerializeField] private GameObject sportEquipment;
+    [Space(10)]
+    [SerializeField] private InputActionReference leftIAR;
+    [SerializeField] private Transform leftControllerFollower;
+    [Space(10)]
+    [SerializeField] private InputActionReference rightIAR;
+    [SerializeField] private Transform rightControllerFollower;
 
     private Rigidbody sportEquipmentRigid;
 
-    private void Start()
+    private void OnEnable()
     {
-        sportEquipmentRigid = sportEquipment.GetComponent<Rigidbody>();
+        leftIAR.action.performed += SwitchController;
+        rightIAR.action.performed += SwitchController;
     }
+
+    private void SwitchController(InputAction.CallbackContext context) => rightControllerActive = !rightControllerActive;
+
+    private void Start() => sportEquipmentRigid = sportEquipment.GetComponent<Rigidbody>();
 
     private void FixedUpdate()
     {
         if (!rightControllerActive)
         {
-            sportEquipmentRigid.MovePosition(leftController.position);
-            sportEquipmentRigid.MoveRotation(leftController.rotation);
+            sportEquipmentRigid.MovePosition(leftControllerFollower.position);
+            sportEquipmentRigid.MoveRotation(leftControllerFollower.rotation);
         }
         else
         {
-            sportEquipmentRigid.MovePosition(rightController.position);
-            sportEquipmentRigid.MoveRotation(rightController.rotation);
+            sportEquipmentRigid.MovePosition(rightControllerFollower.position);
+            sportEquipmentRigid.MoveRotation(rightControllerFollower.rotation);
         }
     }
 }
