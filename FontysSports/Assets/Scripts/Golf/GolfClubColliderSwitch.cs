861ec8e7f6475061969c48bf28a361e0969c9f8f
Robbert Lengton <robbert.lengton@hotmail.nl>
2025-06-13 10:58:26 +0200

    wip

--- FontysSports/Assets/Scripts/Golf/GolfClubColliderSwitch.cs
+++ FontysSports/Assets/Scripts/Golf/GolfClubColliderSwitch.cs
@@ -1,27 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class GolfClubColliderSwitch : MonoBehaviour
 {
-    [SerializeField] private float speedThreshold = 3;
     [SerializeField] private Transform clubHead;
+    [SerializeField] private float speedThreshold = 0.01f;
+    [Space(10)]
     [SerializeField] private Collider bigCollider;
+    [SerializeField] private GameObject testCube;
+    [SerializeField] private float lifeSpan = 0.1f;
 
     private Vector3 clubHeadPreviousPos = Vector3.zero;
-    private bool swapping = false;
-    private bool overSpeedThreshold = false;
+    private bool swapped = false;
 
     private void FixedUpdate()
     {
         Vector3 clubHeadPos = clubHead.position;
         float distance = Vector3.Distance(clubHeadPreviousPos, clubHeadPos);
-        if (overSpeedThreshold != distance >= speedThreshold) swapping = true;
-        overSpeedThreshold = distance >= speedThreshold;
+        bool swapping = false;
+        if (distance >= speedThreshold) swapping = true;
         clubHeadPreviousPos = clubHeadPos;
 
-        if (swapping)
-        {
-            bigCollider.enabled = overSpeedThreshold;
-            swapping = false;
-        }
+        if (!swapped && swapping) StartCoroutine(Swap());
+    }
+
+    private IEnumerator Swap()
+    {
+        swapped = true;
+        bigCollider.enabled = true;
+        testCube.SetActive(true);
+        yield return new WaitForSeconds(lifeSpan);
+        bigCollider.enabled = false;
+        testCube.SetActive(false);
+        swapped = false;
     }
 }
