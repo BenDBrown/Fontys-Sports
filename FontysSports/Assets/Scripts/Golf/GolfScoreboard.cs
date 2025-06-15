3d5796fb60731343f3741b8d6db8c8b682e80572
Benjamin Brown <b.brown@student.fontys.nl>
2025-06-15 21:35:25 +0200

    Score menu

--- FontysSports/Assets/Scripts/Golf/GolfScoreboard.cs
+++ FontysSports/Assets/Scripts/Golf/GolfScoreboard.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class GolfScoreboard : MonoBehaviour
 {
-    // ToDo
+    [SerializeField]
+    GolfPlayerScoreRow[] scoreboardRows;
+
+    public void SetGolfPlayerScores(GolfPlayerScoreInfo[] scores)
+    {
+        for (int i = 0; i < scoreboardRows.Length; i++)
+        {
+            if (i >= scores.Length)
+            {
+                scoreboardRows[i].gameObject.SetActive(false);
+                continue;
+            }
+            scoreboardRows[i].gameObject.SetActive(true);
+            scoreboardRows[i].SetPlayerScoreInfo(scores[i]);
+        }
+    }
 }
